dd3a14eaeb8d2a025f9e0605c66a4fd442179038
Ibrahim-Elnour <ibrahimapplicationmail@gmail.com>
2024-10-02 18:01:38 -0400

    Update MedicationReminderApp.java

--- MedicationReminderApp.java
+++ MedicationReminderApp.java
@@ -28,36 +28,28 @@ public class MedicationReminderApp {
 
         } while (addList);
         System.out.println("Medications added.");
-        // helped past here
         ReminderThread reminderThread = new ReminderThread(medList, timeList);
         reminderThread.start();
     }
-    // need to parse time var for float and pass val as argument into thread.sleep method
-    // step one parse time var into a readable int format
-    // pass into thread.sleep
-    // Custom thread class extending Thread
 static class ReminderThread extends Thread {
     private final ArrayList<String> medList;
     private final ArrayList<Integer> timeList;
 
-    // Constructor
     public ReminderThread(ArrayList<String> medList, ArrayList<Integer> timeList) {
         this.medList = medList;
         this.timeList = timeList;
     }
 
-    // Override the run method
     @Override
     public void run() {
         for (int i = 0; i < medList.size(); i++) {
             try {
-                // Convert minutes to milliseconds (1 minute = 60000 milliseconds)
                 long waitTime = timeList.get(i) * 60000L; 
-                Thread.sleep(waitTime); // Sleep before showing the reminder
+                Thread.sleep(waitTime); 
                 System.out.println("Reminder: It's time to take your medication: " + medList.get(i) + "!");
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
-                break; // Exit if the thread is interrupted
+                break; 
             }
         }
       }
